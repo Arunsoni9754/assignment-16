94c55d683d483d3c0a1ab3bf4894313f1b2d4c2e
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-14 22:24:59 +0530

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b=0,i,j;
+    printf("Enter one array");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(2-i==j)
+            {b+=a[i][j];}
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("%d",b);
+}
